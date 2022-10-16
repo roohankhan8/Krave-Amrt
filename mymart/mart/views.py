7e7d1c48014bf95f88c77924bbbfb6a645a83f6e
roohankhan8 <roohank8@gmail.com>
2022-10-17 02:36:54 +0500

    Fourth

--- mymart/mart/views.py
+++ mymart/mart/views.py
@@ -2,10 +2,14 @@ import re
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+# from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    my_products = Product.objects.all()
+    params = {'product': my_products}
+    return render(request, 'index.html', params)
 def about(request):
     return render(request, 'about.html')
 def contact(request):
